5385aa56be4f2660dab572fbf6dc04c5be5a6381
TW-Arzer <115579050+TW-Arzer@users.noreply.github.com>
2022-10-11 21:57:35 +0200

    Informations

--- 1_3.py
+++ 1_3.py
@@ -1,10 +1,10 @@
 import sys
-import math
+import math #Wird gebraucht um die Funktion math.pi zu nutzen
 
-rayon = float(input("Rayon:"))
+rayon = float(input("Rayon:")) #Float, damit auch Dezimalzahlen als Radius eingegeben werden können
 
 def aire():
-    return rayon * rayon * math.pi
+    return rayon * rayon * math.pi #Exponenten können auch mit '**Exponent', also rayon ** 2 berechnet werden
     
 def perimetre():
     return 2 * rayon * math.pi
