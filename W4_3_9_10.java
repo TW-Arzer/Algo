971cb091866de284cc4a2b2497c2d77ff23988fb
TW-Arzer <115579050+TW-Arzer@users.noreply.github.com>
2022-10-18 08:18:20 +0200

    Korrektur

--- W4_3_9_10.java
+++ W4_3_9_10.java
@@ -3,8 +3,8 @@ public class W4_3_9 {
 	public static void main(String[] args) {
 	
 		String mon_mot = "Hard But Cool!!";
-		String premiere = mon_mot.substring(mon_mot.length()-1);
-		String derniere = mon_mot.substring(12,13);
+		String premiere = mon_mot.substring(0,1);
+		String derniere = mon_mot.substring(mon_mot.length()-1);
 		String lettre_5 = mon_mot.substring(5,6);
 		String lettre_9_13 = mon_mot.substring(9,13);
 		
